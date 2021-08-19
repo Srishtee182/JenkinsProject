00f753e9d01bc67ad505f4492cc4596eb00aebb9
Srishtee Pandey <srishtee.pandey@infostretch.com>
2021-08-19 11:19:06 +0530

    commit thisc

---
+++ src/test/java/sshrunwithparameterpass/JenkinsProject/LaunchBrowser.java
@@ -0,0 +1,11 @@
+package sshrunwithparameterpass.JenkinsProject;
+
+import org.testng.annotations.Test;
+
+public class LaunchBrowser {
+
+	@Test
+	public void firstTest() {
+		System.out.println("This need to be executed");
+	}
+}
---
+++ src/test/java/sshrunwithparameterpass/JenkinsProject/SeconfClass.java
@@ -0,0 +1,11 @@
+package sshrunwithparameterpass.JenkinsProject;
+
+import org.testng.annotations.Test;
+
+public class SeconfClass {
+
+	@Test
+	public void SecondClass() {
+		System.out.println("This is second class");
+	}
+}
